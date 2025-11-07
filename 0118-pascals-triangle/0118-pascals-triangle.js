ea6cfa6192ffa0507042b2c60ab312d009a0a022
Adhil Unnikrishnan  P <adhilunnikrishnanofficial@gmail.com>
2025-11-07 10:34:51 +0530

    Update 0118-pascals-triangle.js

--- 0118-pascals-triangle/0118-pascals-triangle.js
+++ 0118-pascals-triangle/0118-pascals-triangle.js
@@ -6,10 +6,9 @@ var generate = function(numRows) {
       const triangle = [];
 
     for (let i = 0; i < numRows; i++) {
-        const row = new Array(i + 1).fill(1); // first and last elements = 1
+        const row = new Array(i + 1).fill(1); 
 
         for (let j = 1; j < i; j++) {
-            // Each number is sum of two numbers from previous row
             row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
         }
 
