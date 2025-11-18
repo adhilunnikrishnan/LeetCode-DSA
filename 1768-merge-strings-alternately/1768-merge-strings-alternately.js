71fa31e66c45b7eb09789a9ce689173e35802f67
Adhil Unnikrishnan  P <adhilunnikrishnanofficial@gmail.com>
2025-11-18 10:32:25 +0530

    Time: 50 ms (43.14%), Space: 54.2 MB (82.28%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function(word1, word2) {
+     let i = 0, j = 0;
+    let result = "";
+
+    while (i < word1.length && j < word2.length) {
+        result += word1[i++] + word2[j++];
+    }
+
+    return result + word1.slice(i) + word2.slice(j); 
+};
